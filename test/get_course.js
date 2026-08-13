006ba1283fa3c0d9a8ab95ec23b27e63dfba8aa3
Loan <110610727+ejnalo@users.noreply.github.com>
2026-08-13 11:47:55 +0200

    Update get_course.js

--- test/get_course.js
+++ test/get_course.js
@@ -1,12 +1,14 @@
-import { Client } from '../lib/index.js';
+import { Client, GROUPS } from "../lib/index.js";
 
 const client = new Client();
 
-let group_id = "f5c52197-6d54-46ab-a803-079ed115f811"; // ID du groupe
-let course_id = null; // uid du cours (optionnel si course_date est défini)
+let course_id = "f5c52197-6d54-46ab-a803-079ed115f811"; // uid du cours (optionnel si course_date est défini)
 
-client.celcat.getCourse(group_id, course_id).then((course) => {
-    console.log(course);
-}).catch((err) => {
-    console.error(err);
-});
+client.celcat
+	.getCourse(GROUPS.MMI2.A2, { id: course_id })
+	.then((course) => {
+		console.log(course);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
